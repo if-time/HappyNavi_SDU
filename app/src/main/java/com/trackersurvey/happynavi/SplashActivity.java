0b2aeed596b383b6fc65beb1f68fc3976de45009
Dong <690443961@qq.com>
2019-04-05 12:10:19 +0800

    Update SplashActivity.java

--- app/src/main/java/com/trackersurvey/happynavi/SplashActivity.java
+++ app/src/main/java/com/trackersurvey/happynavi/SplashActivity.java
@@ -5,15 +5,26 @@ import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.githang.statusbar.StatusBarCompat;
+import com.huawei.hms.support.api.push.TokenResult;
+import com.trackersurvey.com.huawei.android.hms.agent.HMSAgent;
+import com.trackersurvey.com.huawei.android.hms.agent.common.handler.ConnectHandler;
+import com.trackersurvey.com.huawei.android.hms.agent.push.handler.GetTokenHandler;
+import com.trackersurvey.service.HuaweiPushRevicer;
 import com.trackersurvey.util.Common;
+import com.trackersurvey.util.ToastUtil;
 
-public class SplashActivity extends BaseActivity {
+import static com.trackersurvey.service.HuaweiPushRevicer.ACTION_TOKEN;
+import static com.trackersurvey.service.HuaweiPushRevicer.ACTION_UPDATEUI;
+
+
+public class SplashActivity extends BaseActivity implements HuaweiPushRevicer.IPushCallback  {
 
     private ImageView iv_start;
     protected boolean _active = true;
@@ -37,6 +48,20 @@ public class SplashActivity extends BaseActivity {
         tv_version.setText(Common.getAppVersionName(SplashActivity.this));
 //		Handler x = new Handler();
 //		x.postDelayed(new splashhandler(), 2000);
+
+        /**
+         * SDK连接HMS
+         */
+        HMSAgent.connect(this, new ConnectHandler() {
+            @Override
+            public void onConnect(int rst) {
+                ToastUtil.show(SplashActivity.this,"HMS connect end:" + rst);
+            }
+        });
+
+        getToken();
+
+        registerBroadcast();
 
         AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
         animation.setDuration(2000); // 动画持续时间
@@ -68,5 +93,40 @@ public class SplashActivity extends BaseActivity {
                 finish();
             }
         });
+    }
+
+    private void registerBroadcast() {
+        HuaweiPushRevicer.registerPushCallback(this);
+    }
+
+
+    /**
+     * 获取token
+     */
+    private void getToken() {
+//        ToastUtil.show(SplashActivity.this,"get token: begin");
+        HMSAgent.Push.getToken(new GetTokenHandler() {
+            @Override
+            public void onResult(int rst) {
+//                ToastUtil.show(SplashActivity.this,"get token: end" + rst);
+            }
+        });
+    }
+
+    private static String token = null;
+
+    @Override
+    public void onReceive(Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            Bundle b = intent.getExtras();
+            if (b != null && ACTION_TOKEN.equals(action)) {
+                token = b.getString(ACTION_TOKEN);
+//                Log.i("onReceive get token: ", "onReceive: " + token);
+            } else if (b != null && ACTION_UPDATEUI.equals(action)) {
+                String log = b.getString("log");
+//                ToastUtil.show(SplashActivity.this,"onReceive get token: end" + log);
+            }
+        }
     }
 }
